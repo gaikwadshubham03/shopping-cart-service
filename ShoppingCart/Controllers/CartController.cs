4b0e9dadfd1fa1f4997b08ee1d8632db418a7a6b
Shubham Subhash Gaikwad <ShubhamSubhash.Gaikwad@philips.com>
2024-06-01 02:34:00 -0400

    initial commit

---
+++ ShoppingCart/Controllers/CartController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShoppingCart.Models;
+
+
+namespace ShoppingCart.Controllers
+{
+	[ApiController]
+	[Route("api/[controller]")]
+	public class CartController : ControllerBase
+	{
+		private static List<CartItem> cartItems = new List<CartItem>();
+
+		[HttpPost]
+		public IActionResult AddItem([FromBody] CartItem item)
+		{
+			if (item == null || item.Quantity <= 0)
+			{
+				return BadRequest("Invalid item data");
+			}
+			cartItems.Add(item);
+			return Ok(item);
+		}
+
+
+		[HttpGet]
+		public IActionResult GetItems()
+		{
+			return Ok(cartItems);
+		}
+	}
+}
---
+++ ShoppingCart/Models/CartItem.cs
@@ -0,0 +1,16 @@
+﻿namespace ShoppingCart.Models
+{
+	public class CartItem
+	{
+		public string Name { get; set; }
+		public double Price { get; set; }
+		public int Quantity { get; set; }
+
+		public CartItem(string name, double price, int quantity)
+		{
+			Name = name;
+			Price = price;
+			Quantity = quantity;
+		}
+	}
+}

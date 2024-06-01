6767266e49ef324df0dc02f8d5fd37c7b1ce845f
Shubham Subhash Gaikwad <ShubhamSubhash.Gaikwad@philips.com>
2024-06-01 11:33:45 -0400

    adding data validation in the model

--- ShoppingCart/Models/CartItem.cs
+++ ShoppingCart/Models/CartItem.cs
@@ -1,9 +1,16 @@
-﻿namespace ShoppingCart.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShoppingCart.Models
 {
 	public class CartItem
 	{
+		[Required]
 		public string Name { get; set; }
+
+		[Range(0.01, double.MaxValue, ErrorMessage = "Price must be greater than zero")]
 		public double Price { get; set; }
+
+		[Range(1, int.MaxValue, ErrorMessage = "Item should have atleast 1 quantity")]
 		public int Quantity { get; set; }
 
 		public CartItem(string name, double price, int quantity)
@@ -12,5 +19,7 @@
 			Price = price;
 			Quantity = quantity;
 		}
+
+
 	}
 }
